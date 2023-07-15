9abb81db92dc6095468765e1299e9fc8bae75a94
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2023-07-14 21:09:50 -0300

    Correção em checkout.js

--- server/api/checkout.js
+++ server/api/checkout.js
@@ -1,7 +1,7 @@
 const stripeAPI = require('../stripe');
 
 async function createCheckoutSession(req, res) {
-    const domainUrl = process.env.WER_APP_URL;
+    const domainUrl = process.env.WEB_APP_URL;
     const { line_items, customer_email } = req.body;
     if (!line_items || !customer_email) {
         return res.status(400).json({ Error: 'parâmetros de sessão insuficientes' });
@@ -18,7 +18,7 @@ async function createCheckoutSession(req, res) {
             cancel_url: `${domainUrl}/canceled`,
             shipping_address_collection: { allowed_countries: ['GB', 'US'] }
         });
-        res.status(200).json({ sessionID: session.id });
+        res.status(200).json({ sessionId: session.id });
     } catch (error) {
         console.log(error);
         return res.status(400).json({ Error: 'Ocorreu um erro. Não foi possível criar uma sessão' });
