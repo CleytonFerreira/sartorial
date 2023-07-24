1f16ea4c01184a628f021d35f2b5c5736157726d
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2023-07-24 12:08:03 -0300

    Ajuste em CartIcon

---
+++ frontend/src/components/Cart/CartIcon.jsx
@@ -0,0 +1,26 @@
+import { useContext, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from '../../context/CartContext';
+import cartImg from '../../assets/icons/cart_img.svg';
+
+const CartIcon = () => {
+    const { itemCount } = useContext(CartContext);
+    const [cartState, setCartState] = useState(itemCount);
+
+    useEffect(() => {
+        setCartState(itemCount);
+    }, [itemCount]);
+
+    return (
+        <div>
+            <Link to="/carrinho">
+                <img src={cartImg} alt="carrinho" />
+            </Link>
+            {
+                itemCount > 0 ? <span>{cartState}</span> : null
+            }
+        </div>
+    );
+};
+
+export default CartIcon;
