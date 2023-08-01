78c95eda96fff863d4e870552802f8e22a6b7dc2
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2023-07-31 22:05:05 -0300

    ShippingAddress

---
+++ frontend/src/components/StripeCheckout/CustomCheckout/ShippingAddress.jsx
@@ -0,0 +1,87 @@
+import { Formik } from 'formik';
+import PropTypes from 'prop-types';
+
+const validate = (values) => {
+    const { name, email, address } = values;
+    const errors = {};
+    if (!email) { errors.email = 'Preencha este campo'; }
+    if (!name) { errors.name = 'Preencha este campo'; }
+    if (!address) { errors.address = 'Preencha este campo'; }
+
+    return errors;
+};
+
+const ShippingAddress = ({ setShipping }) => {
+    const initialValues = {
+        email: '',
+        name: '',
+        address: ''
+    };
+
+    return (
+        <div>
+            <h4>Endereço de envio</h4>
+            <Formik
+                initialValues={initialValues}
+                validate={validate}
+                onSubmit={(values) => {
+                    console.log('Valores: ', values);
+                    setShipping(values);
+                }}
+            >
+                {
+                    ({ values, errors, handleChange, handleSubmit }) => {
+                        const { name, email, address } = errors;
+                        return (
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        onChange={handleChange}
+                                        value={values.name}
+                                        className={name ? "error" : ""}
+                                        placeholder="Nome"
+                                    />
+                                </div>
+
+                                <div>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        onChange={handleChange}
+                                        value={values.email}
+                                        className={email ? "error" : ""}
+                                        placeholder="E-mail"
+                                    />
+                                </div>
+
+                                <div>
+                                    <input
+                                        type="text"
+                                        name="address"
+                                        onChange={handleChange}
+                                        value={values.address}
+                                        className={address ? "error" : ""}
+                                        placeholder="Endereço"
+                                    />
+                                </div>
+                                <div>
+                                    <button type="submit">
+                                        CONTINUAR
+                                    </button>
+                                </div>
+                            </form>
+                        );
+                    }
+                }
+            </Formik>
+        </div>
+    );
+};
+
+ShippingAddress.propTypes = {
+    setShipping: PropTypes.func
+};
+
+export default ShippingAddress;
--- frontend/src/pages/CheckoutPage/index.jsx
+++ frontend/src/pages/CheckoutPage/index.jsx
@@ -1,11 +1,15 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
 import Layout from '../../components/shared/Layout';
 import checkoutPageStyle from './CheckoutPage.module.css';
-import StripeCheckout from '../../components/StripeCheckout';
+import ShippingAddress from '../../components/StripeCheckout/CustomCheckout/ShippingAddress';
 
 const CheckoutPage = () => {
     const { itemCount, total } = useContext(CartContext);
+    const [shipping, setShipping] = useState(null);
+    const addressShown = {
+        display: (shipping ? 'none' : 'block')
+    };
 
     return (
         <Layout>
@@ -13,7 +17,9 @@ const CheckoutPage = () => {
                 <h2>Resumo do pedido</h2>
                 <h3>{`Total de itens: ${itemCount}`}</h3>
                 <h4>{`Valor a ser pago: R$${total}`}</h4>
-                <StripeCheckout />
+                <div style={addressShown}>
+                    <ShippingAddress setShipping={setShipping} />
+                </div>
             </div>
         </Layout>
     );
