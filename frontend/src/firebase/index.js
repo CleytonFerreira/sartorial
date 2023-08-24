6225c9b53ff8a68fcdbafa3b1e7ceeedc5200113
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2023-08-23 23:17:52 -0300

    Ajuste em variáveis de ambiente no firebase

--- frontend/src/firebase/index.js
+++ frontend/src/firebase/index.js
@@ -1,19 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-import dotenv from 'dotenv';
-dotenv.config({path: './.env'})
 
-const config = {
-  apiKey: import.meta.env.VITE_APP_API_KEY,
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_APP_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_APP_ID
+const firebaseConfig  = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-const firebaseApp = initializeApp(config);
+const firebaseApp = initializeApp(firebaseConfig );
 const firestore = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
 
